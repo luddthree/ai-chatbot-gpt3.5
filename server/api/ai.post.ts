6cb1a42f25087da2ac26c64fa2f467dd94868bf1
luddthree~ <luddetv@gmail.com>
2023-12-11 15:55:15 +0100

    filter

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,5 +1,6 @@
 
 import { Configuration, OpenAIApi } from "openai";
+import { customerSupportAgent } from "~~/agents";
 export default defineEventHandler(async (event) => {
 
   const body = await readBody(event);
@@ -15,6 +16,7 @@ const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
   messages: body.messages || [],
   temperature: body.temperature || 1,
+  ...customerSupportAgent(body)
 });
 return completion.data;
 });
